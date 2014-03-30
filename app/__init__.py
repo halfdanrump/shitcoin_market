95efc58dba9447a61d6197394a24ad8ce5b65c4f
Halfdan Rump <dalekk@gmail.com>
2014-03-30 09:02:55 +0900

    Fixes logging and adds RotatingFileHandler

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,22 @@ from redis import Redis
 from market.orderbook import Orderbook
 
 app = Flask(__name__)
-print __name__
 app.redis = Redis()
 app.config['order_queue'] = 'my_queue'
 
-# import logging.config, yaml
-# config = yaml.load(open('log_conf.yaml'))
-# logging.config.dictConfig(config)
+import logging.config, yaml
+config = yaml.load(open('app/config/log_conf.yaml'))
+logging.config.dictConfig(config)
 # l = logging.getLogger(__name__)
 # l.error('TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEEEEE')
 
-import logging
-error_handler = logging.FileHandler('errors.log')
-error_handler.setLevel(logging.ERROR)
-app.logger.addHandler(error_handler)
+# from logging.handlers import RotatingFileHandler
+# import logging
+
+# handler = RotatingFileHandler('logs/app.log', maxBytes = 10000)
+# handler.setLevel(logging.INFO)
+# app.logger.addHandler(handler)
+
 
 book = Orderbook()
 
