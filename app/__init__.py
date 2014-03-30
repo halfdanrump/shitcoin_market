00bd19978b5fc2af3e75b33a7aec2113ac276bc5
Halfdan Rump <dalekk@gmail.com>
2014-03-30 12:12:51 +0900

    Adds app config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,14 @@ from redis import Redis
 from market.orderbook import Orderbook
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+### Redis setup
 app.redis = Redis()
 app.config['order_queue'] = 'my_queue'
 
+
+### Create market objects
 book = Orderbook()
 
 from app import views, market
