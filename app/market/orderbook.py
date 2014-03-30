c33e1402045391a29cd92d900c38a7a133f7f83a
Halfdan Rump <dalekk@gmail.com>
2014-03-30 09:06:01 +0900

    Fixes logging, moves or queue func to ob class. Various other changes.

--- app/market/orderbook.py
+++ app/market/orderbook.py
@@ -2,13 +2,29 @@ import Queue
 import logging
 
 
-from flask import current_app
+from flask import Flask, current_app
 from pickle import loads, dumps
 
 from uuid import uuid4
 
 import multiprocessing
+import heapq
+from bisect import insort
 
+### Logging setup
+
+import logging.config, logging.handlers, yaml
+config = yaml.load(open('app/config/log_conf.yaml'))
+logging.config.dictConfig(config)
+
+logger = logging.getLogger(__name__)
+logger.setLevel('DEBUG')
+handler = logging.handlers.RotatingFileHandler('app/logs/orderbook.log', maxBytes = 10**6)
+#handler = logging.FileHandler('app/logs/orderbook.log')
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 class DelayedResult(object):
@@ -25,31 +41,18 @@ class DelayedResult(object):
         return self._rv
 
 
-def queuefunc(f):
-    def queue_order(order):
-        qkey = current_app.config['order_queue']
-        key = '%s:result:%s' % (qkey, str(uuid4()))
-        s = dumps((key, order))
-        print current_app.redis.rpush(current_app.config['order_queue'], s)
-        return DelayedResult(key)
-    f.queue_order = queue_order
-    return f
 
 
 # Make decorator for catching exceptions and writing them to log
-
-def error_handler(f, *args):
-	logger = current_app.logger
-	def run():
+import functools
+def error_handler(f, *args, **kwargs):
+	@functools.wraps(f) ### Necessary to update the module of the decorated function so that flask detects the right application context
+	def run_safe(*args, **kwargs):
 		try:
-			f(*args)
+			f(*args, **kwargs)
 		except Exception, e:
 			logger.error('Error detected: %s'%e)
-	return run
-
-# class test():
-# 	def __init(self):
-# 		self.p = multiprocessing.Process
+	return run_safe
 
 
 
@@ -59,73 +62,102 @@ class Orderbook():
 
 	def __init__(self):
 		self.ID += 1
-		self.buy_orders = Queue.PriorityQueue()
-		self.sell_orders = Queue.PriorityQueue()
-		self.logger = logging.getLogger(__name__)
+		self.buy_orders = list()
+		self.sell_orders = list()
+		
 	
 	def start_auction(self, app):
-		
+		""" Run this to start the auction. If the auction is already running, nothing happens"""
 		if not hasattr(self, 'daemon'):
 			self.daemon = multiprocessing.Process(name = 'auction_%s'%self.ID, target = self.queue_daemon, args = (app,))		
 			self.daemon.start()
-			self.logger.info('Starting auciton for orderbook %s'%self.ID)
+			logger.info('Starting auciton for orderbook %s'%self.ID)
+
+	
+	def queue_order(self, order):
+		qkey = current_app.config['order_queue']
+		key = '%s:result:%s' % (qkey, str(uuid4()))
+		s = dumps((key, order))
+		current_app.redis.rpush(current_app.config['order_queue'], s)
+		return DelayedResult(key)
+
 
 	def queue_daemon(self, app, rv_ttl=500):
+		""" The daemon that listens for incoming orders. Must be run in a separate process. """
 		while True:
 			msg = app.redis.blpop(app.config['order_queue'])
-			self.logger.info('Received order')
 			key, order = loads(msg[1])
+			logger.debug('Received order: %s'%order)
 			try:
-				rv = self.process_order(order)
+				response = self.process_order(order)
 			except Exception, e:
-				rv = e
-			if rv is not None:
-				app.redis.set(key, dumps(rv))
+				current_app.logger.exception(e)
+				response = e
+			if response is not None:
+				app.redis.set(key, dumps(response))
 				app.redis.expire(key, rv_ttl)
 
-	@queuefunc
+	#@error_handler
 	def process_order(self, new_order):
 		while True:
 			if new_order.current_volume == 0:
+				logger.debug('Order volume depleted: %s'%new_order.ID)
 				break
+
 			matching_order = self.get_matching_order(new_order)
+			
+			logger.debug('Matching order: %s'%matching_order)
 			if matching_order == None:
-				#error_handler(self.add_order)(new_order)
-				try:
-					self.add_order(new_order)
-				except Exception, e:
-					self.logger.exception('Could not add order')
+				self.add_order(new_order)
 				break
 			else:
 				self.execute_transaction(new_order, matching_order)
-			
+				if matching_order.current_volume > 0:
+				self.add_order(matching_order)
 
-	import heapq
 
 	def get_matching_order(self, new_order):
-		matching_order = None
-		if new_order.is_buy():
-			try:
-				if new_order.price > self.sell_orders[0].price:
-					matching_order = heapq.heappop(self.sell_orders)
-			except IndexError:
-				self.logger.exception('PROBLEM')
-		elif new_order.is_sell():
-			pass
-		return None
-
+		if new_order.is_buy() and self.has_sell_orders():				
+			if new_order.price > self.best_sell_price():
+				return heapq.heappop(self.sell_orders)
+		elif new_order.is_sell() and self.has_buy_orders():
+			if new_order.price < self.best_buy_price():
+				return heapq.heappop(self.buy_orders)
+		else:
+			return None
+		
 	#@error_handler
 	def add_order(self, new_order):
-		self.logger.error('Adding order')
+		logger.debug('Adding order %s'%order)
 		if new_order.is_limit():
 			if new_order.is_buy():
-				self.buy_orders.put((new_order.price, new_order))
+				insort(self.buy_orders, new_order)
 			elif new_order.is_sell():
-				self.sell_orders.put((new_order.price, new_order))
+				insort(self.sell_orders, new_order)
 		else:
-			self.logger.debug('Discarding market order')
-		self.logger.error('Standing orders: %s, %s'%(self.buy_orders.qsize(), self.sell_orders.qsize()))
+			logger.debug('Discarding market order')
+		logger.debug('Standing orders: %s, %s'%(len(self.buy_orders), len(self.sell_orders)))
 
+
+	def has_buy_orders(self):
+		if len(self.buy_orders) > 0: return True
+		else: return False
+
+	def has_sell_orders(self):
+		if len(self.sell_orders) > 0: return True
+		else: return False
+
+	def best_buy_price(self):
+		try:
+			return self.buy_orders[0].price
+		except IndexError:
+			return None
+
+	def best_sell_price(self):
+		try:
+			return self.sell_orders[0].price
+		except IndexError:
+			return None
 
 	def execute_transaction(self, new_order, matching_order):
 		pass
