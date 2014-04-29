72287e2d417268ce41caec28ef93500c0b00d352
Halfdan Rump <dalekk@gmail.com>
2014-04-29 14:04:49 +0900

    Adds javascript libraries

---
+++ app/static/javascript/utils.js
@@ -0,0 +1,16 @@
+var utils = {
+	myFunction: function(p1, p2) {
+	    return p1 * p2;              // the function returns the product of p1 and p2
+	}
+
+	// print_orderbook: function(book){
+	// 	var b = '<tr><th>Sell volume</th><th>Price</th><th>Buy volume</th></tr>';
+	// 	$.each(jQuery.parseJSON(book['sell_side']), function(index, tuple){
+	// 	b += '<tr><td>' + tuple[1] + '</td><td>' + tuple[0] + '</td><td></td></tr>';
+	// 	});
+	// 	$.each(jQuery.parseJSON(book['buy_side']), function(index, tuple){
+	// 	b += '<tr><td></td><td>' + tuple[0] + '</td><td>' + tuple[1] +'</td></tr>';
+	// 	});
+	// 	$('#orderbook_table').html(b);
+	// }
+};
