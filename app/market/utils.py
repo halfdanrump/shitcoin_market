7d22b706b888b486b55f68b1a62a11b82cbf4fe7
Halfdan Rump <dalekk@gmail.com>
2014-05-06 17:44:09 +0900

    Adds decorator to initalize test environment

--- app/market/utils.py
+++ app/market/utils.py
@@ -14,6 +14,20 @@ def get_random_order(side = None):
 	order = Order(**{'volume':volume, 'price':price, 'type':Order.LIMIT, 'side':side, 'received':received})
 	return order
 
+def get_random_order2(**kwargs):
+	volume = round(abs(random.gauss(100, 30)))
+	price = round(abs(random.gauss(100, 30)))
+	if random.random() > 0.5:
+		side = Order.BUY
+	else:
+		side = Order.SELL
+	received = datetime.utcnow()
+	order_args = {'volume':volume, 'price':price, 'type':Order.LIMIT, 'side':side, 'received':received}
+	order_args = order_args.update(kwargs)
+	order = Order(**order_args)
+	return order
+
+
 def get_many_random_orders(n_buy, n_sell):
 	buy_orders = [get_random_order(Order.BUY) for i in range(n_buy)]
 	sell_orders = [get_random_order(Order.SELL) for i in range(n_sell)]
--- app/testing/orderbook.py
+++ app/testing/orderbook.py
@@ -4,7 +4,9 @@ from test_utils import clear_redis_keys
 from app.market.orderbook import Orderbook
 
 class BasicOrderbookTests(unittest.TestCase):
-	
+	"""
+	Basic tests for adding and removing orders
+	"""	
 	@clear_redis_keys
 	def setUp(self):
 		print 'Create books and orders etc...'
