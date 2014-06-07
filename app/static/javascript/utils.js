c93c18ce60c9e753e48002c442b79edf7ac17947
Halfdan Rump <dalekk@gmail.com>
2014-06-07 12:24:30 +0900

    Moves javascript function into external file

--- app/static/javascript/utils.js
+++ app/static/javascript/utils.js
@@ -1,16 +1,28 @@
-var utils = {
-	myFunction: function(p1, p2) {
-	    return p1 * p2;              // the function returns the product of p1 and p2
-	}
+// var utils = {
+// 	myFunction: function(p1, p2) {
+// 	    return p1 * p2;              // the function returns the product of p1 and p2
+// 	}
 
-	// print_orderbook: function(book){
-	// 	var b = '<tr><th>Sell volume</th><th>Price</th><th>Buy volume</th></tr>';
-	// 	$.each(jQuery.parseJSON(book['sell_side']), function(index, tuple){
-	// 	b += '<tr><td>' + tuple[1] + '</td><td>' + tuple[0] + '</td><td></td></tr>';
-	// 	});
-	// 	$.each(jQuery.parseJSON(book['buy_side']), function(index, tuple){
-	// 	b += '<tr><td></td><td>' + tuple[0] + '</td><td>' + tuple[1] +'</td></tr>';
-	// 	});
-	// 	$('#orderbook_table').html(b);
-	// }
-};
+// 	// print_orderbook: function(book){
+// 	// 	var b = '<tr><th>Sell volume</th><th>Price</th><th>Buy volume</th></tr>';
+// 	// 	$.each(jQuery.parseJSON(book['sell_side']), function(index, tuple){
+// 	// 	b += '<tr><td>' + tuple[1] + '</td><td>' + tuple[0] + '</td><td></td></tr>';
+// 	// 	});
+// 	// 	$.each(jQuery.parseJSON(book['buy_side']), function(index, tuple){
+// 	// 	b += '<tr><td></td><td>' + tuple[0] + '</td><td>' + tuple[1] +'</td></tr>';
+// 	// 	});
+// 	// 	$('#orderbook_table').html(b);
+// 	// }
+// };
+
+function get_orderbook_html(book){
+    	var b = '<tr><th>Sell volume</th><th>Price</th><th>Buy volume</th></tr>';
+      $.each(jQuery.parseJSON(book['sell_side']), function(index, tuple){
+        b += '<tr><td>' + tuple[1] + '</td><td>' + tuple[0] + '</td><td></td></tr>';
+      });
+      $.each(jQuery.parseJSON(book['buy_side']), function(index, tuple){
+        b += '<tr><td></td><td>' + tuple[0] + '</td><td>' + tuple[1] +'</td></tr>';
+      });
+      
+      return b;
+    };
