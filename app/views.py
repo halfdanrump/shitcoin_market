fb214e4ad2b2a43f04a550735f628c1f5d642b90
Halfdan Rump <dalekk@gmail.com>
2014-06-12 01:45:30 +0900

    openid login still does not work

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import flapp, socketio, oid
-from flask import render_template, redirect, flash, g, session, request, url_for
+from flask import render_template, redirect, flash, g, session, request, url_for, flash
 from forms import OrderForm, UserLoginForm, UserRegisterForm
 from app.dbase.models import User
 from app import logger
@@ -17,48 +17,9 @@ def lookup_current_user():
 		g.user = User.query.filter_by(openid = openid).first()
 	print 'CURRENT USER:', g.user
 
-from flask import flash
-
-
-@oid.after_login
-def create_or_login(response):
-	session['openid'] = response.identity_url
-	logger.debug( 'OpenID login with identity_url: %s'%response.identity_url )
-	user = User.query.filter_by(openid=response.identity_url).first()
-	if user:
-		g.user = user
-		logger.debug( 'User signed in %s'%user )
-		# flash(u'Successfully signed in')
-		return redirect(oid.get_next_url())
-	else:
-		logger.debug( 'User not registered in system: %s'%response.identity_url )
-		register_form = UserRegisterForm()
-		if register_form.validate_on_submit():
-			g.user = User.create(openid = response.identity_url, **form.data)
-			logger.debug( 'Created user %s'%g.user )
-		return render_template("create_profile.html", identity_url = response.identity_url, register_form = register_form)
-		# return redirect( url_for( 'create_profile', next=oid.get_next_url(), identity_url = response.identity_url) )
-
-
-# @flapp.route('/create_profile', methods = ['GET', 'POST'])
-# def create_profile():
-# 	if g.user:# or 'openid' not in session:
-# 		logger.debug( 'Redicredting user %s to home screen...'%g.user)
-# 		return redirect(url_for('home'))
-# 	else:
-# 		logger.debug( 'Redirecting unknown user to create profile page %s'%identity_url[])
-# 		return render_template("create_profile.html")
-
-
-@flapp.route('/', methods = ['GET'])
-def index():
-	return redirect(url_for('login'))
-
-
 @flapp.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
 def login():
-	login_form = UserLoginForm()
 	if g.user is not None:
 		return redirect( oid.get_next_url() )
 	
@@ -67,12 +28,67 @@ def login():
 		if openid: 
 			return oid.try_login(openid, ask_for=['email'])
     
-	return render_template('login.html', next=oid.get_next_url(), error=oid.fetch_error(), login_form = login_form)
-	# if login_form.validate_on_submit():
-	# 	return redirect('/home')
+	return render_template('login.html', next=oid.get_next_url(), error=oid.fetch_error())
 
-	# return render_template("login.html", login_form = login_form, providers = flapp.config['OPENID_PROVIDERS'])
 
+
+
+@oid.after_login
+def create_or_login(response):
+	session['openid'] = response.identity_url
+	logger.debug( 'OpenID login with identity_url: %s'%response.identity_url )
+	user = User.query.filter_by(openid=response.identity_url).first()
+	logger.debug( 'Matched openid with registered user: %s'%user )
+	if user is not None:
+		g.user = user
+		logger.debug( 'User signed in %s'%user )
+		flash(u'Successfully signed in')
+		logger.debug( 'Redirecting to URL: %s'%oid.get_next_url())
+		return redirect(oid.get_next_url())
+	# else:
+	# 	logger.debug( 'User not registered in system: %s'%response.identity_url )
+	
+	# register_form = UserRegisterForm()
+	
+	# if register_form.validate_on_submit():
+	# 	g.user = User.create(openid = response.identity_url, **form.data)
+	# 	logger.debug( 'Created user %s'%g.user )
+	# # return render_template("create_profile.html", identity_url = response.identity_url, register_form = register_form)
+	return redirect( url_for( 'create_profile', next=oid.get_next_url(), identity_url = response.identity_url) )
+
+import uuid
+@flapp.route('/create_profile', methods = ['GET', 'POST'])
+def create_profile():
+	register_form = UserRegisterForm()
+	logger.debug('IN CREATE PROFILE1')
+	logger.debug('OPENID: %s'%session['openid'])
+	logger.debug('GLOBAL USER: %s'%g.user)
+	if g.user is not None or 'openid' not in session:
+		logger.debug( 'Redicredting user %s to home screen...'%g.user)
+		return redirect(url_for('home'))
+	
+	if register_form.validate_on_submit():
+		new_user = User.create(openid = uuid.uuid4().hex, name = 'Halfdan')
+		logger.debug( 'Creating new user %s'%new_user )
+		g.user = new_user
+		logger.debug( oid.get_next_url())
+		return redirect( oid.get_next_url())
+	logger.debug('IN CREATE PROFILE2')
+	return render_template("create_profile.html", register_form = register_form, next = oid.get_next_url())
+
+
+@flapp.route('/submit_form', methods = ['POST'])
+def fuckface():
+	logger.debug('FUCKFACE')
+	return render_template('dummy.html')
+
+
+@flapp.route('/', methods = ['GET'])
+def index():
+	return redirect(url_for('login'))
+
+
+	
 
 
 
